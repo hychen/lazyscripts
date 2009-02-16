8d153350dafc083ba6aab690c774a657a5c9c685
billy3321 <billy3321@gmail.com>
2009-02-17 00:55:11 +0800

    Add sync_repo.sh, use to sync with others.

---
+++ sync_repo.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Date: 2009/2/17
+# Author: billy3321
+# A simple script use to sync your repo with others.
+#
+
+function push_repo (){
+while true
+do
+read -p "Pull success. Continue push to your repo?(Y/n)" PUSH_REPO
+  case $PUSH_REPO in
+   "Y"|"y"|"")
+   git push && echo "Push success."
+   break
+   ;;
+   "N"|"n")
+   echo "Stop push."
+   break
+   ;;
+   *)
+   echo "Please enter a choice."
+   ;;
+   esac
+done
+}
+
+
+echo "Whos repo you want to sync?"
+echo "1. hychen"
+echo "2. yurenju"
+echo "3. billy3321"
+echo "Please enter your choice:"
+read -p "What do you want to do now? Please enter the number:" ACT
+  case $ACT in
+   "1")
+   git pull git://github.com/hychen/lazyscript.git && push_repo
+   ;;
+   "2")
+   git pull git://github.com/yurenju/lazyscript.git && push_repo
+   ;;
+   "3")
+   git pull git://github.com/billy3321/lazyscript.git && push_repo
+   ;;
+   *)
+   echo "Please enter a number."
+   ;;
+   esac
+
+
+#END
