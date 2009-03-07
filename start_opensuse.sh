bf6e4f04021a626dbd4d087ea2ba3a41ef7f1c49
雨蒼 <billy3321@219-68-216-205.adsl.dynamic.giga.net.tw>
2009-03-07 22:26:41 +0800

    merge start_opensuse_i386 and start_opensuse_x86_64

--- start_opensuse.sh
+++ start_opensuse.sh
@@ -8,15 +8,33 @@
 # Please run as root.
 
 
-export ARCH_NAME="`uname -a | cut -d " " -f 12`"
+export ARCH_NAME="`uname -i`"
 
 echo "正在下載並安裝lazyscripts執行所需的套件...."
 
 zypper install git git-core python-setuptools
 
+case $ARCH_NAME in
+
+i386)
+
 zypper install http://lazyscripts.googlecode.com/files/python-nose-0.10.4-3.1.i586.rpm
 
 zypper install http://lazyscripts.googlecode.com/files/python-git-0.1.6-3.1.i586.rpm
+;;
+
+x86_64)
+
+zypper install http://lazyscripts.googlecode.com/files/python-nose-0.10.4-3.1.x86_64.rpm
+
+zypper install http://lazyscripts.googlecode.com/files/python-git-0.1.6-3.1.x86_64.rpm
+;;
+
+*)
+
+echo "抱歉，lazyscripts並不支援 ${ARCH_NAME} 作業系統平台。"
+;;
+esac
 
 echo "執行完畢！即將啟動lazyscripts..."
 
