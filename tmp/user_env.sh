e71f9f15536859c6113c3e6d61b698af53435f3a
Yuren Ju <yurenju@yurenju-laptop.(none)>
2009-04-22 23:50:30 +0800

    add tmp/user_env.sh

---
+++ tmp/user_env.sh
@@ -0,0 +1,4 @@
+# this file will contain user environment variables after sudo
+# because execute "sudo" on ubuntu will lost all user env variables
+# so we need this file to take some environment variables after sudo
+# this file will re-write when you execute, generate code in slzs.
