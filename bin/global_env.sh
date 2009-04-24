0d792b6dde278e08659a762cf10e9c8c29d37fad
Yuren Ju <yurenju@yurenju-laptop.(none)>
2009-04-24 23:38:59 +0800

    switch back git-python from easy_install to dpkg -i install.

--- bin/global_env.sh
+++ bin/global_env.sh
@@ -3,6 +3,7 @@
 export DISTRO_ID=`lsb_release -is`
 export DISTRO_CODENAME=`lsb_release -cs`
 export DISTRO_VERSION=`lsb_release -rs`
+
 case $(getconf LONG_BIT) in
     "32")
     export PLAT_NAME="i386"
@@ -11,5 +12,6 @@ case $(getconf LONG_BIT) in
     export PLAT_NAME="x86_64"
     ;;
 esac
+
 export WGET="wget --tries=2 --timeout=120 -c"
 
--- bin/package_debian_ubuntu.sh
+++ bin/package_debian_ubuntu.sh
@@ -8,6 +8,9 @@
 
 function install_require_packages () {
     show_text="正在下載並安裝Lazyscripts執行所需的套件...."
+    google_code_url="http://lazyscripts.googlecode.com"
+    git_python_filename="python-git_0.1.6-1_all.deb"
+
     if [ "$1"!="gui" ]; then
         echo "$show_text"
     fi
@@ -26,7 +29,9 @@ function install_require_packages () {
             python-nose make > /dev/null 2>&1;
 
         echo 90;
-        easy_install GitPython > /dev/null 2>&1;
+        wget -c "$google_code_url/files/$git_python_filename" > /dev/null 2>&1;
+        dpkg -i "$git_python_filename" > /dev/null 2>&1;
+        rm "$git_python_filename" > /dev/null 2>&1;
         echo 100;
         ) | zenity --auto-close --auto-kill --progress --text="$show_text"
     else #if slzs console, use console mode
@@ -34,13 +39,15 @@ function install_require_packages () {
         apt-get -y --force-yes install \
             git-core python-setuptools \
             python-nose make > /dev/null
-        easy_install GitPython > /dev/null
+        wget -c "$google_code_url/files/$git_python_filename"
+        dpkg -i "$git_python_filename"
+        rm "$git_python_filename"
     fi
     echo "執行完畢！即將啟動Lazyscripts..."
 }
 
 echo "Check for required packsges..."
-if dpkg -l python-nose python-setuptools git-core > /dev/null 2>&1 ; then
+if dpkg -l python-nose python-setuptools git-core python-git > /dev/null 2>&1 ; then
     echo "Require packages installed."
 else
     echo "Require packages not installed."
