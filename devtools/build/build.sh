e7478889c4bc0185e8c28ce0c398040d10ea87d4
Yuren Ju <yurenju@yurenju-laptop.(none)>
2009-05-10 18:13:03 +0800

    add default scriptspoll and fixbug for can't execute build.sh on any path

--- devtools/build/build.sh
+++ devtools/build/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+relative_path="`dirname $0`"
+
+cd "$relative_path"
 CURRENT_PWD="`pwd`"
-cd "$CURRENT_PWD"
 
 read -p "Please type version: " VERSION
 filename="lazyscripts-$VERSION.tar.gz"
@@ -11,6 +13,15 @@ cd ../..
 git archive --format=tar master | gzip -9 > "lazyscripts.tar.gz"
 mv "lazyscripts.tar.gz" "$CURRENT_PWD"
 cd "$CURRENT_PWD"
+
+mkdir -p "temp"
+cd "temp"
+tar zxf ../lazyscripts.tar.gz
+cp -a ../../../scriptspoll/464df77ba280ba7a885291be2653b7da scriptspoll/
+tar czf lazyscripts.tar.gz *
+cp lazyscripts.tar.gz ..
+cd ..
+rm -rf temp
 
 # build self-extracting file from tarball
 gcc `pkg-config gtk+-2.0 --cflags --libs` -o sfx sfx.c
