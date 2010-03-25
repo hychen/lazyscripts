f69d3acafdb0878299013a7245911a2f3660de27
Hsin Yi Chen (hychen) <ossug.hychen@gmail.com>
2010-03-25 23:50:21 +0800

    * Fix message typo of graphic interface..
    Conflicts:
    
    	lazyscripts/gui.py

--- lazyscripts/gui.py
+++ lazyscripts/gui.py
@@ -125,7 +125,7 @@ class ToolPage:
         col.set_attributes (render, active=0)
         view.append_column (col)
 
-        col = gtk.TreeViewColumn (_("items"))
+        col = gtk.TreeViewColumn (_("Items"))
         render=gtk.CellRendererText ()
         col.pack_start (render)
         col.set_attributes (render, markup=1)
@@ -183,7 +183,7 @@ class FinalPage:
         hbox=gtk.HBox(False, 0)
         term=vte.Terminal()
         term.set_scrollback_lines(65535)    # Will this value be too big?
-        term.feed(_('execute action: \n'))
+        term.feed(_('Execute action: \n'))
         term.set_cursor_blinks(True)
         self.term=term
         scroll=gtk.VScrollbar( term.get_adjustment() )
@@ -352,7 +352,7 @@ class MainWin:
 
     #{{{def confirm_close(self):
     def confirm_close(self):
-        if self.complete or query_yes_no(_('do you want to quit lazyscripts?'), self.win):
+        if self.complete or query_yes_no(_('Do you want to quit lazyscripts?'), self.win):
             gtk.main_quit()
             return True
         return False
